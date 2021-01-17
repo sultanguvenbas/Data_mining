2b50cb77ce759f5ce8a0acf9d2b2fddfa4d1defd
U370709069 <akgol97_@hotmail.com>
2021-01-17 21:08:40 +0300

    Reading data-set and taking needed attributes from data-set which are temp rh wind and rain. Also splitting the data into 2 part as we learned in class assignments

--- finalProject.py
+++ finalProject.py
@@ -1,1 +1,19 @@
-print("denemee")
+import pandas as pd
+import numpy as np
+
+# Reading dataset file
+filename = 'forestfires.csv'
+df = pd.read_csv(filename)
+
+#Taking temp	RH	wind	rain from end
+data = df[df.columns[-5:]].values
+np.random.shuffle(data)
+X = data[:, :-1]  # temp	RH	wind	rain
+y = data[:, -1]  # area
+
+# Splitting train and test dataset
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+print(X)
+print(y)
