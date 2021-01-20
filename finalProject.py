0e896c4e337921584823f8631b21a5fcba98b4e5
U170709055 <sultanguvenbas@posta.mu.edu.tr>
2021-01-20 22:37:50 +0300

    took area and month attributes from the data-set to be able to draw total burned area for each months

--- finalProject.py
+++ finalProject.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from scipy.optimize import least_squares
+import matplotlib.pyplot as plt
 
 from sklearn.neural_network import MLPRegressor
 import warnings
@@ -15,7 +16,7 @@ warnings.filterwarnings("ignore")
 filename = 'forestfires.csv'
 df = pd.read_csv(filename)
 
-#Taking temp	RH	wind	rain from end
+#Taking temp,RH,wind,rain and area
 data = df[df.columns[-5:]].values
 np.random.shuffle(data)
 X = data[:, :-1]  # temp	RH	wind	rain
@@ -78,3 +79,43 @@ print('Training set rmse: %.3f' % rmse(y_train, y_predict))
 
 y_predict = model(X_test, *params)
 print('Test set rmse: %.3f' % rmse(y_test, y_predict))
+
+def histogram():
+
+    data2 = pd.read_csv(filename)
+    months = data2[data2.columns[2]].values
+    area = data2[data2.columns[-1]].values
+    monthDict = {}
+
+    for monthName in range(len(months)):
+        if months[monthName] not in monthDict:
+            monthDict[months[monthName]] = 0
+
+    for areaindex in range(len(area)):
+        monthDict[months[areaindex]] += area[areaindex]
+    #print(monthDict)
+
+    monthArray = ["jan","feb","mar","apr","may","jun","jul",
+                  "aug","sep","oct","nov","dec"]
+
+    arr=[]
+    arrValue=[]
+
+    for checkMont in monthArray:
+        for keys, values in monthDict.items():
+            if checkMont == keys:
+                arr.append(keys)
+                arrValue.append(values)
+
+    #print(monthDict)
+    #print(arr)
+    #print(arrValue)
+
+    plt.xlabel("Months")
+    plt.ylabel("Total Burned Area (in ha)")
+    plt.title("Histogram")
+    plt.bar(arr,arrValue)
+    plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.7)
+    plt.show()
+
+histogram()
