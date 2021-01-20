ab07513f887600cdb111bbafe3c35c546768662d
U370709069 <akgol97_@hotmail.com>
2021-01-20 21:28:13 +0300

    defined  Levenberg-Marquardt function and do the needed calculation inside of it. And also defined 'func' to call it in least_squares. Lastly, Printed Least Squares Model with Levenberg-Marquardt algorithm

--- finalProject.py
+++ finalProject.py
@@ -4,14 +4,18 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from scipy.optimize import least_squares
+from sklearn.neural_network import MLPRegressor
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # Reading dataset file
-from sklearn.neural_network import MLPRegressor
 
 filename = 'forestfires.csv'
 df = pd.read_csv(filename)
 
-#Taking temp	RH	wind	rain from end
+# Taking temp,RH,wind,rain and area
 data = df[df.columns[-5:]].values
 np.random.shuffle(data)
 X = data[:, :-1]  # temp	RH	wind	rain
@@ -21,12 +25,30 @@ y = data[:, -1]  # area
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-#print(X)
-#print(y)
+
+# print(X)
+# print(y)
 
 def rmse(y_true, y_predict):  # Root mean Square Error
     retval = mean_squared_error(y_true, y_predict)
     return math.sqrt(retval)
+
+
+# Defining Levenberg-Marquardt Model
+def model(x, a, b, c, d, e):
+    # 0 -> temp
+    # 1 -> RH
+    # 2 -> wind
+    # 3 -> rain
+
+    top = (x[:, 0] * a) ** 2 + (x[:, 2] * c) ** 2
+    bottom = (x[:, 1] * b) ** 2 + (x[:, 3] * d) ** 2
+    return (top / bottom) + e
+
+
+def func(params, xdata, ydata):
+    return ydata - model(xdata, *params)
+
 
 # Activation alternatives: 'identity', 'logistic', 'tanh', 'relu'
 print('MLPRegressor Model')
@@ -40,4 +62,19 @@ y_predict = mdl.predict(X_train)
 print('Training set rmse: %.3f' % rmse(y_train, y_predict))
 
 y_predict = mdl.predict(X_test)
+print('Test set rmse: %.3f' % rmse(y_test, y_predict))
+
+print()
+x0 = np.ones(5)  # taking ones
+print('Least Squares Model with Levenberg-Marquardt algorithm')
+
+result = least_squares(func, x0, args=(X_train, y_train), method='lm')
+
+params = result.x
+# Printing RMSE of training and test dataset for MLP Model Levenberg-Marquardt
+
+y_predict = model(X_train, *params)
+print('Training set rmse: %.3f' % rmse(y_train, y_predict))
+
+y_predict = model(X_test, *params)
 print('Test set rmse: %.3f' % rmse(y_test, y_predict))
